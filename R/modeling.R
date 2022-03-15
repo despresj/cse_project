d4b25ccdd06dfadf69e54a6f5665e65e5c3ac717
despresj <54477023+despresj@users.noreply.github.com>
2022-03-15 16:51:38 -0400

    add modeling checks

--- R/modeling.R
+++ R/modeling.R
@@ -1,6 +1,6 @@
 library(rstan) # observe startup messages
 options(mc.cores = parallel::detectCores())
-
+set.seed(69)
 df <- read.csv("data/clean/wine_quality.csv")
 train <- sample(1:nrow(df), size = floor(nrow(df) * 0.8), replace = FALSE)
 df_train <- df[train,]
@@ -15,7 +15,7 @@ wine_data <- list(
   N_train = nrow(X_train),
   N_test = nrow(X_test),
   K = length(unique(df_train$quality)),
-  D = ncol(X),
+  D = ncol(X_train),
   y_train = as.numeric(factor(df_train$quality)),
   y_test = as.numeric(factor(df_test$quality)),
   X_train = X_train,
@@ -24,6 +24,18 @@ wine_data <- list(
 
 fit <- stan("stan/multi_logit.stan",
             data=wine_data,
-            chains = 8, iter = 250, warmup = 50)
+            chains = 8,
+            iter = 250,
+            warmup = 100
+            )
 print(fit)
-traceplot(fit)
+
+beepr::beep()
+traceplot(fit, pars=paste0("beta[", 1:13, "]"))
+
+params <- extract(fit)
+y <- params$y_test 
+yhat <- apply(y, 2, DescTools::Mode) + 4
+
+mean(abs(yhat - df_test$quality))
+ 
